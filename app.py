0812806e50ad1c23852024490ad6762b1ca246f5
Perumall <96647299+Perumallpardhiv@users.noreply.github.com>
2024-05-24 19:05:40 +0530

    Update outputs

--- app.py
+++ app.py
@@ -11,11 +11,12 @@ app = Flask(__name__)
 model = YOLO('best.pt')
 classes = ['Crack' ,'Dent']
 
-def image_to_base64(image_path):
-    with Image.open(image_path) as image:
-        buffered = io.BytesIO()
-        image.save(buffered, format="PNG")
-        return base64.b64encode(buffered.getvalue()).decode("utf-8")
+def image_to_base64(image_data):
+    img = Image.fromarray(image_data)
+    with io.BytesIO() as buffer:
+        img.save(buffer, format="PNG")
+        base64_str = base64.b64encode(buffer.getvalue()).decode('utf-8')
+    return base64_str
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -28,17 +29,19 @@ def predict():
     results = model.predict(Image.open(io.BytesIO(image.read())))
     print("Model predicted")
     
-    output_image_path = 'result.jpg'
-    for result in results:
+    # output_image_path = 'result.jpg'
+    print(results)
+    print(results.__len__() , "results found")
+    # for result in results:
         # result.show()  # display to screen
-        result.save(filename=output_image_path)
+        # result.save(filename=output_image_path)
     
-    base64_str = image_to_base64(output_image_path)
+    base64_str = image_to_base64(results[0].orig_img)
+    print(base64_str)
     print("Image converted to base64")
-    # print(base64_str)
     # print(results)
     # return jsonify({'image': base64_str})
-    return jsonify({'success': "success"})
+    return jsonify({'success': "success", 'base64': base64_str})
     # return send_file(output_image_path, mimetype='image/jpeg')
 
 @app.route('/')
